2fc92edd94f4dc60f80bfad69a52889fd61988fe
TangliziGit <tanglizimail@foxmail.com>
2020-01-14 13:21:01 +0800

    init scala devloping env

---
+++ src/main/scala/me/tanglizi/persona/App.scala
@@ -0,0 +1,5 @@
+package me.tanglizi.persona
+
+object App extends App {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/me/tanglizi/persona/AppTest.scala
@@ -0,0 +1,14 @@
+package me.tanglizi.persona
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+}
+
+
