66b794c1f5a79c1557bcde88c4683aaf051176c4
TangliziGit <tanglizimail@foxmail.com>
2020-01-23 21:38:32 +0800

    restructure, using redis to get computed data

---
+++ src/main/scala/me/tanglizi/persona/enum/Entity.scala
@@ -0,0 +1,5 @@
+package me.tanglizi.persona.`enum`
+
+object Entity extends Enumeration {
+  val USER, POST, REPLY, COMMENT = Value
+}
---
+++ src/main/scala/me/tanglizi/persona/enum/UserGroup.scala
@@ -0,0 +1,5 @@
+package me.tanglizi.persona.`enum`
+
+object UserGroup extends Enumeration {
+  val ALL, MALE, FEMALE, ADULT, NOT_ADULT = Value
+}
